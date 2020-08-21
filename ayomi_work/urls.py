1c6329ca00a74e3f7f5ca2f47938f6443b1cc54d
quentinrinaldi <quentin.rinaldi@gmail.com>
2020-08-21 14:02:51 +0200

    signup ok

--- ayomi_work/urls.py
+++ ayomi_work/urls.py
@@ -9,7 +9,7 @@ from . import views
 urlpatterns = [
     path('userAccount/login/', auth_views.LoginView.as_view(redirect_authenticated_user=True), name='login'),
     path('userAccount/logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('userAccount/profile', login_required(TemplateView.as_view(template_name='profile.html'))),
+    path('userAccount/profile', login_required(TemplateView.as_view(template_name='profile.html')), name="profile"),
     path('userAccount/', include('django.contrib.auth.urls')),
     path('signup/', views.signup, name='signup')
 
--- ayomi_work/views.py
+++ ayomi_work/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from .forms import SignupForm
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -13,5 +14,5 @@ def signup(request):
             login(request, user)
             return redirect('home')
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'signup.html', {'form': form})
