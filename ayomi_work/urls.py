1c1a8bed059babc940506d87ebd3fef1f8350fef
quentinrinaldi <quentin.rinaldi@gmail.com>
2020-08-23 21:39:31 +0200

    "/" redirection to login

--- ayomi_work/urls.py
+++ ayomi_work/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
 from . import views
 
 
@@ -12,6 +13,6 @@ urlpatterns = [
     path('userAccount/profile', login_required(TemplateView.as_view(template_name='profile.html')), name="profile"),
     path('userAccount/', include('django.contrib.auth.urls')),
     path('userAccount/update_profile', views.update_profile, name="update_profile"),
-    path('signup/', views.signup, name='signup')
-
+    path('signup/', views.signup, name='signup'),
+    path('', RedirectView.as_view(pattern_name='login', permanent=False))
 ]
